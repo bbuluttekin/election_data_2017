0849a23ce1440c8d7a305d1de8a1306457c3f201
bbuluttekin <bbuluttekin@hotmail.com>
2017-05-11 12:39:45 +0100

    changed sample size operation

--- get_opinion_pools.py
+++ get_opinion_pools.py
@@ -20,7 +20,7 @@ print('Getting year 2017')
 rows_2017 = 0
 for i, row in enumerate(tables[0].find_all('tr')):
     if len(row) != 23:
-        
+
         continue
     if i == 0:
         df_opinion = pd.DataFrame(columns=[[_.text for _ in row.find_all('th')] + ['Year']])
@@ -51,19 +51,19 @@ for i, row in enumerate(tables[2].find_all('tr')):
     df_opinion.loc[i + 2000] = [_.text for _ in row.find_all('td')] + [int(2015)]
     rows_2015 += 1
 
-    
+
 # Logic Validation
 if rows_2015 + rows_2016 + rows_2017 == len(df_opinion):
     print('Download Successful!')
 else:
     print('Data inconsistent!')
-    
-    
+
+
 # Get rid of special characters
 for i in df_opinion.columns[3:-1]:
     df_opinion[i] = ([_.split('%')[0] for _ in df_opinion[i]])
-    
-df_opinion['Sample size'] = [int("".join([_.split(',')[0], _.split(',')[1]])) for _ in df_opinion['Sample size']]
+
+df_opinion['Sample size'] = [int("".join(_.split(','))) for _ in df_opinion['Sample size']]
 
 
 #Remove NA substitutes
